b4bd043b81e56fcae032c965847d9c6397f8da7a
Acacio Cruz <acacio@acacio.com>
2023-01-04 23:58:12 +0000

    [S] Update modules; fix handling on unpadded keys

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/acacio/totp-token
 go 1.17
 
 require (
-	github.com/sec51/convert v0.0.0-20190309075348-ebe586d87951
+	github.com/sec51/convert v1.0.2
 	github.com/sec51/cryptoengine v0.0.0-20180911112225-2306d105a49e
-	github.com/sec51/gf256 v0.0.0-20160126143050-2454accbeb9e // indirect
 	github.com/sec51/qrcode v0.0.0-20160126144534-b7779abbcaf1
-	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
-	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
-	google.golang.org/protobuf v1.27.1
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/sec51/gf256 v0.0.0-20160126143050-2454accbeb9e // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 )
--- totp-token.go
+++ totp-token.go
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright 2021 Acacio Cruz acacio@acacio.coom
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,15 +17,17 @@ limitations under the License.
 package main
 
 import (
-	"github.com/acacio/totp-token/twofactor"
 	"github.com/acacio/totp-token/secrets"
+	"github.com/acacio/totp-token/twofactor"
+	"os"
+	"strings"
 
 	"encoding/base32"
-	"os/user"
-	"io/ioutil"
-	"fmt"
-	"log"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/user"
 
 	"google.golang.org/protobuf/encoding/prototext"
 )
@@ -38,17 +40,21 @@ func CHECK(e error) {
 
 var verbose *bool
 
-func parseArgs() (string) {
+func parseArgs() string {
 
 	keyPtr := flag.String("k", "", "Google Authenticator base32 secret")
 	domainPtr := flag.String("d", "", "Use domain from ~/.totp-keys")
 	verbose = flag.Bool("v", false, "Verbose.")
 	flag.Parse()
 
-  key := "undef"
-  switch {
-		case *keyPtr != "": { key = *keyPtr }
-		case *domainPtr != "": {
+	key := "undef"
+	switch {
+	case *keyPtr != "":
+		{
+			key = *keyPtr
+		}
+	case *domainPtr != "":
+		{
 			usr, err := user.Current()
 			CHECK(err)
 			data, err := ioutil.ReadFile(usr.HomeDir + "/.totp-keys")
@@ -57,31 +63,47 @@ func parseArgs() (string) {
 			if err := prototext.Unmarshal(data[:], totps); err != nil {
 				log.Fatalln("Failed to parse ~/.totp-keys:", err)
 			}
-			for _, secret  := range totps.Secrets {
+			for _, secret := range totps.Secrets {
 				if secret.Domain == *domainPtr {
 					key = secret.Key
 					break
 				}
 			}
 		}
-		default: log.Fatal("You must provide either a key OR a domain in ~/.totp-keys")
+	default:
+		log.Fatal("You must provide either a key OR a domain in ~/.totp-keys")
 	}
 	if key == "undef" {
 		log.Fatal("ERROR: Key not defined.")
 	}
-  return key
+	return key
 }
 
 func main() {
 
 	key := parseArgs()
 
-	if *verbose { fmt.Println("Key: " + key) }
+	if *verbose {
+		fmt.Println("Key: " + key)
+	}
 	binkey, err := base32.StdEncoding.DecodeString(key)
-	CHECK(err)
+	if err != nil {
+		if len(key) != 32 { // Sometimes keys miss their padding.
+			// Try again after adding ==== padding at the end.
+			newkey := key + strings.Repeat("=", 32-len(key))
+			binkey, err = base32.StdEncoding.DecodeString(newkey)
+		}
+		if err != nil {
+			fmt.Println("%v - please try to check your key", err)
+			os.Exit(2)
+		}
+	}
 
 	otp, err := twofactor.NewTOTPFromKey(binkey, "acacio@interarma.com", "InterArma", 6)
-	CHECK(err)
+	if err != nil {
+		fmt.Println("Error generating TOTP: %v", err)
+		os.Exit(3)
+	}
 
 	token := twofactor.CalculateTOTP(otp, 0)
 	fmt.Println(token)
